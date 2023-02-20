2bcb6818b39ca96d327be317833f7f1d49917861
anas <anasmomin064@gmail.com>
2023-02-20 20:19:26 +0530

    assigment 18/02/2023

--- src/app/Components/products/products.component.ts
+++ src/app/Components/products/products.component.ts
@@ -1,22 +1,73 @@
 import { ApiService } from './../../services/api.service';
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
-  constructor(private as: ApiService) {}
+  addToCart: any = [];
+  cols: any;
+  temp: any;
+  Products: any = [];
+  gridByBreakpoint: any = {
+    xl: 3,
+    lg: 3,
+    md: 3,
+    sm: 2,
+    xs: 1,
+  };
+  constructor(
+    private as: ApiService,
+    private breakpointObserver: BreakpointObserver
+  ) {
+    this.breakpointObserver
+      .observe([
+        Breakpoints.XSmall,
+        Breakpoints.Small,
+        Breakpoints.Medium,
+        Breakpoints.Large,
+        Breakpoints.XLarge,
+      ])
+      .subscribe((result) => {
+        if (result.matches) {
+          if (result.breakpoints[Breakpoints.XSmall]) {
+            this.cols = this.gridByBreakpoint.xs;
+          }
+          if (result.breakpoints[Breakpoints.Small]) {
+            this.cols = this.gridByBreakpoint.sm;
+          }
+          if (result.breakpoints[Breakpoints.Medium]) {
+            this.cols = this.gridByBreakpoint.md;
+          }
+          if (result.breakpoints[Breakpoints.Large]) {
+            this.cols = this.gridByBreakpoint.lg;
+          }
+          if (result.breakpoints[Breakpoints.XLarge]) {
+            this.cols = this.gridByBreakpoint.xl;
+          }
+        }
+      });
+  }
 
   ngOnInit(): void {
     this.as.requestProducts().subscribe(
       (data) => {
-        console.log(data);
+        this.temp = data;
+        this.Products = this.temp.products;
       },
       (err) => {
         console.log(err);
       }
     );
+  }
+
+  handleBuyNow(item: any) {
+    console.log('item in buy now', item);
+  }
+  handleAddToCart(item: any) {
+    this.addToCart.push(item);
+    console.log('items in add to cart', this.addToCart);
   }
 }
