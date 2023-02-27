0117408f338878d7bd971eb1dbcd775340545139
Abdul Raheman Anas <109565032+Anasmomin143@users.noreply.github.com>
2023-02-27 22:12:41 +0530

    27/02/2023 class final

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
 import { PRODUCTS_API_RESPONSE, Product } from './../Models/app.model';
 import { Observable, Subject } from 'rxjs';
 import { environment } from './../../environment/environment';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHandler, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -9,18 +9,24 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   constructor(private http: HttpClient) {}
-
+  userDetails:any = JSON.parse(localStorage.getItem('userDetails') as string);
   dataStream = new Subject<any>();
   requestLogin(value: any) {
     return this.http.post(`${environment.api}/auth/login`, value);
   }
   requestProducts(): Observable<PRODUCTS_API_RESPONSE> {
-    return this.http.get(`${environment.api}/products`) as any;
+    return this.http.get(`${environment.api}/products`, {
+      headers: new HttpHeaders().append('token', this.userDetails.token),
+    }) as any;
   }
   requestProductDetailsById(id: any): Observable<Product> {
-    return this.http.get(`${environment.api}/products/${id}`) as any;
+    return this.http.get(`${environment.api}/products/${id}`, {
+      headers: new HttpHeaders().append('token', this.userDetails.token),
+    }) as any;
   }
   requestUpdateUserDetails(id: any, newdata: any): Observable<any> {
-    return this.http.put(`${environment.api}/users/${id}`, newdata);
+    return this.http.put(`${environment.api}/users/${id}`, newdata, {
+      headers: new HttpHeaders().append('token', this.userDetails.token),
+    });
   }
 }
